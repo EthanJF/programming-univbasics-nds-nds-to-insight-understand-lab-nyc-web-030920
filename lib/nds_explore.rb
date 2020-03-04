e62e35b9d6a76c3ce01d6a8aa926d92de6d6ccef
Ethan Fournier <ethanjosephfournier@gmail.com>
2020-03-04 19:21:20 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -13,7 +13,7 @@ def print_first_directors_movie_titles
   movies = directors_database[0][:movies]
   i = 0
   while i < movies.length do
-    puts movies[i]
+    puts movies[i][:title]
     i++
   end
 end
